f6890b82e5404fd5cfc48fcd474eca2f88115044
viktorpodevyn <52883951+viktorpodevyn@users.noreply.github.com>
2021-02-24 09:06:11 +0100

    up until evolutions

--- pokedex.js
+++ pokedex.js
@@ -2,15 +2,15 @@ document.getElementById("pokeball").addEventListener("click", function () {
 
     const userInput = document.querySelector(".search-text").value;
 
-    let apiUrl = `https://pokeapi.co/api/v2/pokemon/${userInput}`; // api url
+    let apiUrl = `https://pokeapi.co/api/v2/pokemon/${userInput}`;
 
-    const names = document.querySelector("#nameBox"); // pokemon name
-    const bigImage = document.querySelector("#picture"); // pokemon image
+    const names = document.querySelector("#nameBox");
+    const bigImage = document.querySelector("#picture");
     const pokeId = document.querySelector("#IDBox");
 
     const allMoves = document.querySelector("#pokeMovements");
-    let prevEvolutionimg = document.querySelector('.evo1'); //image of the pokemon
-    let evolu = document.getElementById('Evolutions');
+    let prevEvolutionimg = document.querySelector('#evo1');
+    let evolution = document.getElementById('Evolutions');
     fetch(apiUrl).then(res => {
         return res.json()
             .then((data, index) => {
@@ -19,8 +19,6 @@ document.getElementById("pokeball").addEventListener("click", function () {
                 names.textContent = data["name"]
                 bigImage.src = data["sprites"]["front_default"];
                 pokeId.textContent = "pokemon ID: " + data["id"]
-
-                console.log(data);
 
                 for (let i = 0; i < 4; i++) {
                     index = i
@@ -32,30 +30,29 @@ document.getElementById("pokeball").addEventListener("click", function () {
                 let four = allMoves.slice(0, 4)
                 four.textContent = data["moves"];
 
-                function evolutionPokemon(evopokemon) {
+                function evolutionPokemon() {
                     fetch(`https://pokeapi.co/api/v2/pokemon-species/${userInput}`)
                         .then(response => response.json())
                         .then(evopokemon => {
-                            console.log(evopokemon)
 
-                            if (evopokemon.evolves_from_species !== null) {
-                                let innerEvol = evopokemon.evolves_from_species.name;
-                                evolu.textContent = innerEvol;
+                            if (evopokemon.evochain !== null) {
+                                let innerEvol = evopokemon.evochain.name;
+                                evolution.textContent = innerEvol;
 
 
                                 function internImage() {
                                     fetch(`https://pokeapi.co/api/v2/pokemon/${innerEvol}`)
                                         .then(response => response.json())
                                         .then(response => {
-                                            console.log(response);
-                                            prevEvolutionimg.evo1 = data["sprites"]["front_default"];
+                                            prevEvolutionimg.src = data["sprites"]["front_default"];
+
                                         })
                                 }
 
                                 internImage()
 
                             } else {
-                                evolu.textContent = 'No previous evolution';
+                                evolution.textContent = 'No previous evolution';
                                 prevEvolutionimg.setAttribute("src", "");
                             }
                         });
